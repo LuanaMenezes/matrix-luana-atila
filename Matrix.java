c07679d03ab759783e597f8d57fc27ddf9a5ab58
LuanaMenezes <luhhh.m@gmail.com>
2015-09-29 13:17:34 -0400

    First file - test

---
+++ Matrix.java
@@ -0,0 +1,37 @@
+public class Matrix {
+    private double m1, m2;
+    public void Matrix()
+    {
+        
+    
+    }
+    
+    public void setSize()
+    {
+        System.out.println("Size of matrix one:");
+        System.out.println("Size of matrix two:");
+        
+    
+    }
+    
+    public void setValues()
+    {
+    
+        
+        
+    }  
+    
+    public void multiplyMatrix()
+    {
+        
+    
+    }
+
+    @Override
+    public String toString()
+    {
+
+        return s;
+    }
+
+}
