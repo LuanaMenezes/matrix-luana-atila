cc796cb7245e95657caf72c5191c343bdcddb419
LuanaMenezes <luhhh.m@gmail.com>
2015-09-30 20:51:53 -0400

    This code already reads the matrix .
    But, appears this exception -> Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: 8
    
    We need to fix it.

--- Matrix.java
+++ Matrix.java
@@ -5,10 +5,16 @@
  */
 package matrix;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  *
@@ -16,64 +22,47 @@ import java.io.IOException;
  */
 public class Matrix {
     
-    public void readFile(String f) throws java.io.FileNotFoundException,java.io.IOException{ 
-     
-        try {
-        File fil = new File("c:/matrix.txt");
-        FileReader dataFile = new FileReader(fil);
-        BufferedReader in = new BufferedReader(dataFile);
+   
+    public static double[][] readFile(Path path) throws IOException
+    {
+            List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
+            
+            int NR_OF_COLUMNS = 9;
+            int NR_OF_ROWS = lines.size();
 
-        String m = in.readLine();
+            double[][] result = new double[NR_OF_ROWS][NR_OF_COLUMNS];
 
-        int size = m.length();
-
-        String [][] matrix = new String [size][size];
-
-        double d = 0;
-        String line = m;
-        String[] split = m.split(", ");    
-        
-       /* for(int i = 0; i < split.length; i++)
-        { 
-             d = Double.parseDouble(split[i]);
-             matrix[0][i] = d;
-        } */
-               
-          for (int j = 0; j < 8; j++)
-          {
-              
-               matrix[0][j] = String.valueOf(split[j]);
-          }
-          for (int i = 1; (line = in.readLine()) != null; ++i)
-          {
-                for (int j = 0; j < 8; j++) 
-                {
-                   matrix[i][j] = String.valueOf(line.charAt(j));
-                }
-          }
-
-           for (int i = 0; i < 8; i++) 
-           {
-                for (int j = 0; j < 8; j++) 
-                {
-                   System.out.print(matrix[i][j] + " ");
-                }
-             System.out.println("");
-           }
-
-        }
-        catch (IOException e1)
+             for(int rowIndex = 0; rowIndex < NR_OF_ROWS; rowIndex++)
+             {
+                 String lines2 =  lines.get(rowIndex).replace(",", "");
+                 List<String> strings = Arrays.asList(new String[]{lines2});
+                 String[] tokens = strings.get(0).split("\\s+");   //split every line
+                 for(int columnIndex = 0; columnIndex < NR_OF_COLUMNS; columnIndex++)
+                 result[rowIndex][columnIndex] = Double.parseDouble(tokens[columnIndex]);   //convert every token to an integer
+             }
+            
+              return result;
+    }
+    
+    public static void display(double m[][])
+    {
+        for(int i =0; i<9; i++)
         {
-            e1.printStackTrace();
+            for(int j=0; j<9; j++)
+            {
+                System.out.print(m[i][j] + " ");
+            }
+            
+            System.out.println("\n");
         }
-}  
+    }
     
     public static void main(String[] args) throws IOException
     {        
         Matrix matrixIO = new Matrix();
-        matrixIO.readFile("c:/matrix.txt");
-       
+        Path p = Paths.get("c:/matrix.txt");
+        double matrix[][];
+        matrix = matrixIO.readFile(p);
+        display(matrix);
     }
   }
-    
-
